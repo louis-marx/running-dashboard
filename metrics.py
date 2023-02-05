7c9b78f9d72f8d8b12c03a05a82e0dc8e6891534
louis-marx <louis.marx@mailo.com>
2023-02-05 14:41:50 +0000

    Add gitpod configuration file

--- metrics.py
+++ metrics.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import datetime as dt
+import math
 from utils import decimal_to_time, week_to_dates, month_to_dates
 
 format_dict = {
@@ -8,47 +10,44 @@ format_dict = {
 }
 
 def get_runs_stat(df, time, unit):
-    if time == df[unit].min():
-        runs_delta = None
-    else:
-        runs_delta = int(df[df[unit] == time].start.item() - df[df[unit] == time - 1].start.item())
-        sign = "+" if runs_delta >= 0 else ""
-        runs_delta = f'{sign}{runs_delta}'
-    runs = df[df[unit] == time].start
-    return runs,runs_delta,"normal"
+    runs = len(df[df[unit] == time])
+    runs_delta = len(df[df[unit] == time]) - len(df[df[unit] == time-1])
+    sign = "+" if runs_delta >= 0 else ""
+    runs_delta = f'{sign}{runs_delta}'
+    return runs, runs_delta, "normal"
 
 def get_distance_stat(df, time, unit):
-    if time == df[unit].min():
-        distance_delta = None
-    else:
-        kilometers_delta = df[df[unit] == time].distance.item() - df[df[unit] == time - 1].distance.item()
-        sign = "+" if kilometers_delta >= 0 else ""
-        distance_delta = f'{sign}{round(kilometers_delta,2)} km'
-    distance = df[df[unit] == time].distance.round(2)
-    return distance, distance_delta,"normal"
+    distance = df[df[unit] == time].distance.sum().round(2)
+    distance_delta = df[df[unit] == time].distance.sum() - df[df[unit] == time - 1].distance.sum()
+    sign = "+" if distance_delta >= 0 else ""
+    distance_delta = f'{sign}{round(distance_delta,2)} km'
+    return distance, distance_delta, "normal"
 
 def get_pace_stat(df, time, unit):
-    if time == df[unit].min():
-        pace_delta = None
+    current_pace = df[df[unit] == time].duration.sum()/df[df[unit] == time].distance.sum()
+    previous_pace = df[df[unit] == time-1].duration.sum()/df[df[unit] == time-1].distance.sum()
+    if not(math.isnan(current_pace)):
+        h, m, s = decimal_to_time(current_pace)
+        pace = "%d'%02d\"" % (m, s)
+        if not(math.isnan(previous_pace)):
+            pace_delta = current_pace - previous_pace
+            sign = "+" if pace_delta >= 0 else "-"
+            hd, md, sd = decimal_to_time(abs(pace_delta))
+            pace_delta = "%s%d'%02d\"" % (sign, md, sd)
+        else:
+            pace_delta = None
     else:
-        pace_delta = df[df[unit] == time].pace.item() - df[df[unit] == time - 1].pace.item()
-        sign = "+" if pace_delta >= 0 else "-"
-        hd, md, sd = decimal_to_time(abs(pace_delta))
-        pace_delta = "%s%d'%02d\"" % (sign, md, sd)
-    h, m, s = decimal_to_time(df[df[unit] == time].pace)
-    pace = "%d'%02d\"" % (m, s)
+        pace = "-"
+        pace_delta = None
     return pace, pace_delta, "inverse"
 
 def get_duration_stat(df, time, unit):
-    if time == df[unit].min():
-        duration_delta = None
-    else:
-        duration_delta = df[df[unit] == time].duration.item() - df[df[unit] == time - 1].duration.mean()
-        sign = "+" if duration_delta >= 0 else "-"
-        hd, md, sd = decimal_to_time(abs(duration_delta))
-        duration_delta = "%s%d:%02d:%02d" % (sign, hd, md, sd)
-    h, m, s = decimal_to_time(df[df[unit] == time].duration)
+    h, m, s = decimal_to_time(df[df[unit] == time].duration.sum())
     duration = "%d:%02d:%02d" % (h, m, s)
+    duration_delta = df[df[unit] == time].duration.sum() - df[df[unit] == time - 1].duration.sum()
+    sign = "+" if duration_delta >= 0 else "-"
+    hd, md, sd = decimal_to_time(abs(duration_delta))
+    duration_delta = "%s%d:%02d:%02d" % (sign, hd, md, sd)
     return duration, duration_delta, "normal"
 
 def get_metrics(df, metric, time, unit):
@@ -64,14 +63,14 @@ def get_metrics(df, metric, time, unit):
         return None
 
 def get_options(df, unit):
-    options = [*reversed(range(df[unit].min(), df[unit].max()+1))]
+    options = [*reversed(range(df[unit].min(), (dt.date.today().isocalendar()[1]+13)%52+1))]
     nb = min(7,len(options))
     return options[:nb]
 
 def display_metrics(df, unit):
     #Get the running metrics per time unit
-    df_agg = df.groupby([unit], as_index=False).agg({'start': 'count', 'distance': 'sum', 'duration': 'sum'})
-    df_agg['pace'] = df_agg.duration / df_agg.distance
+    # df_agg = df.groupby([unit], as_index=False).agg({'start': 'count', 'distance': 'sum', 'duration': 'sum'})
+    # df_agg['pace'] = df_agg.duration / df_agg.distance
 
     #Let the user choose which time to display
     options = get_options(df, unit)
@@ -81,7 +80,7 @@ def display_metrics(df, unit):
     cols = st.columns(len(metrics))
 
     for i in range(len(cols)):
-        metric, delta, direction = get_metrics(df_agg, metrics[i], option, unit)
+        metric, delta, direction = get_metrics(df, metrics[i], option, unit)
         cols[i].metric(metrics[i], metric, delta, direction)
 
     return option
